0c55517ef415e271e9102be9eb6b666f1b8b6dc4
Ryan Drew <ryan@thedrews.org>
2018-05-23 16:08:57 -0600

    Update FallingObjectNode to use view factory

--- js/falling-objects/view/FallingObjectNode.js
+++ js/falling-objects/view/FallingObjectNode.js
@@ -10,7 +10,6 @@ define( function( require ) {
 
   // modules
   var fallingObjects = require( 'FALLING_OBJECTS/fallingObjects' );
-  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   // var Property = require( 'AXON/Property' );
@@ -19,30 +18,25 @@ define( function( require ) {
    * Constructor for FallingObjectNode
    *
    * @param {FallingObject} fallingObject - the corresponding FallingObject for this Node
+   * @param {FallingObjectViewFactory} fallingObjectViewFactory - view factory for constructing images for this Node
    * @param {ModelViewTransform2} modelViewTransform - the coordinate transform between model coordinates and view coordinates
    * @constructor
    */
-  function FallingObjectNode( fallingObject, modelViewTransform ) {
+  function FallingObjectNode( fallingObject, fallingObjectViewFactory, modelViewTransform ) {
 
-    // Get reference to this context for use in other contexts
-    var self = this;
+    // Set attributes
     this.fallingObject = fallingObject;
+    this.fallingObjectViewFactory = fallingObjectViewFactory;
     this.modelViewTransform = modelViewTransform;
 
     // Call super constructor
     Node.call( this );
 
     // Set image for the node
-    this.image = new Image( fallingObject.image, {
-      centerX: 0,
-      centerY: 0
-    } );
+    this.image = this.fallingObjectViewFactory.constructView( this.fallingObject.name, this.modelViewTransform );
     this.addChild( this.image );
 
-    // Scale to appropriate size
-    this.scale( fallingObject.imageScale );
-
-    // Set drop position
+    // Set drop position based on fallingObject's position
     this.translation = modelViewTransform.modelToViewPosition( fallingObject.positionProperty.get() );
   }
 
