bcc3ba21553fef200f639b8f81293804d456cb16
Ryan Drew <ryan@thedrews.org>
2018-07-18 07:46:12 -0600

    Added clouds to the MovingBackground (WIP)

--- js/common/view/MovingBackground.js
+++ js/common/view/MovingBackground.js
@@ -12,7 +12,10 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var cloudImage = require( 'image!FALLING_OBJECTS/cloud.png' );
   var fallingObjects = require( 'FALLING_OBJECTS/fallingObjects' );
+  var FallingObjectsConstants = require( 'FALLING_OBJECTS/common/FallingObjectsConstants' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LinearGradient = require( 'SCENERY/util/LinearGradient' );
   var Node = require( 'SCENERY/nodes/Node' );
@@ -22,16 +25,24 @@ define( function( require ) {
   /**
    * Construct the background. Most of the work done here is to create a skeleton that will
    * be filled in and sized in the 'layout' function.
+   *
+   * @param {FallingObjectsModel} fallingObjectsModel - will be used to pull selected falling object
    */
-  function MovingBackground( ) {
+  function MovingBackground( fallingObjectsModel ) {
 
     // Call the super
     Node.call( this );
+    
+    // Get a reference to model
+    this.fallingObjectsModel = fallingObjectsModel;
 
     // Create a rectangular sky
     this.sky = new Rectangle( 0, 0, 0, 0 );  // don't know screen dimensions yet, so sizing for later
-
     this.addChild( this.sky );
+
+    // Create a container node for the clouds
+    this.cloudContainerNode = new Node();
+    this.addChild( this.cloudContainerNode );
 
   }
 
@@ -44,13 +55,101 @@ define( function( require ) {
      * object's altitude. The sky's dimensions should be set prior to calling this method.
      * TODO: Will be linked to altitude at a later date, for now this is constant
      */
-    updateSkyGradient: function( ) {
+    updateSkyGradient: function() {
 
       // Just set to a constant gradient for now
       // Use the height of the sky node rectangle to determine the height of the linear gradient
       this.sky.setFill( new LinearGradient( 0, 0, 0, this.sky.height ).addColorStop( 0, '#02ace4' ).addColorStop( 1, '#cfecfc' ) );
 
     },
+
+    /**
+     * Return a valid x-coordinate for a cloud node
+     *
+     * @param {number} centerX - center X coordinate of the screen
+     * @param {boolean} placeOnLeft - if true then the cloud will be placed to the left of the center, and if false then to the right
+     */
+    genCloudNodeXPos: function( centerX, placeOnLeft ) {
+      //TODO: should be adjusted to x can cover a larger area
+
+      // Define these constants for convenience
+      var cloudMarginX = FallingObjectsConstants.MB_CLOUD_MARGIN_X;
+      var cloudMarginXVariance = FallingObjectsConstants.MB_CLOUD_MARGIN_X_VARIANCE;
+
+      // Determine spacing between the cloud node and the center of the screen
+      // Use the base margin and apply a variant factor, which ranges from 0 to cloudMarginXVariance
+      var padding = cloudMarginX + ( cloudMarginXVariance * Math.random() );
+      // If the cloud is on the left then the padding is negative, otherwise positive
+      if ( placeOnLeft ) {
+        padding *= -1;
+      }
+
+      // Return the new xCoordinate for the cloud
+      return centerX + padding;
+
+    },
+
+    /**
+     * Return a valid scale for a cloud node
+     */
+    genCloudNodeScale: function() {
+
+      // Define these constants here for convenience
+      var scaleRange = FallingObjectsConstants.MB_CLOUD_SCALE_RANGE;
+
+      // The scale of the cloud is a random float in the above scaleRange
+      return ( Math.random() * scaleRange.getLength() ) + scaleRange.min;
+
+    },
+
+    /**
+     * Create a new CloudNode to add into the background. Will have a random initial scale and x
+     * coordinate- both of which will be reset when the cloud reaches the top of the screen.
+     *
+     * @param {number} initialCenterX - initial coordinate of the X center of the cloud node
+     * @param {number} initialTopY - initial coordinate of the top of the cloud node
+     * @param {number} initialScale - initial scale of the cloud node
+     */
+    createCloudNode: function( initialCenterX, initialTopY, initialScale ) {
+
+      // First create a new image that will act as our node
+      var cloudNode = new Image( cloudImage );
+      // get a reference to this
+      var self = this;
+
+      /**
+       * Override the reset method of the node for repositioning when the user resets the sim. 
+       * This also acts as a layout method, as it sets the cloud in its initial position
+       */
+      cloudNode.reset = function() {
+        cloudNode.scale( initialScale );
+        cloudNode.setCenterX( initialCenterX );
+        cloudNode.setTop( initialTopY );
+      }
+
+      // Link the cloudNode's position with the selected falling object's position property
+      this.fallingObjectsModel.selectedFallingObject.positionProperty.lazyLink( function( newPosition, oldPosition ) {
+
+        // Animate the cloud to move upwards so the selected falling object appears to fall downwards
+        var dy = ( ( newPosition.y - oldPosition.y ) * cloudNode.getScaleVector().y )
+        cloudNode.setCenterY( cloudNode.getCenterY() + dy );
+
+        // If we hit the top, then reset
+        // TODO: Ensure cloud is placed in proper order so it lies either behind or in front of other clouds depending on scale
+        if ( cloudNode.bottom < self.sky.top ) {
+          cloudNode.scale( self.genCloudNodeScale() );
+          cloudNode.setCenterX( self.genCloudNodeXPos() );
+          cloudNode.setTop( self.sky.bottom );
+        }
+
+      } );
+
+      cloudNode.reset();  // call reset to layout initial position
+
+      return cloudNode;
+
+    },
+
 
     /**
      * Layout nodes on the moving background
@@ -63,6 +162,11 @@ define( function( require ) {
      */
     layout: function( offsetX, offsetY, width, height, scale ) {
 
+      // If we already have cloud nodes created on the screen, then just return- don't need to create more
+      if ( this.cloudContainerNode.children.length > 0 ) {
+        return;
+      }
+
       // Determine center of the screen for convenience in the following calculations
       var center = new Vector2( ( -offsetX + ( width / scale - offsetX ) ) / 2, ( -offsetY + ( height / scale - offsetY ) ) / 2 );
 
@@ -72,6 +176,52 @@ define( function( require ) {
       // And now set the sky's color
       // TODO: Move this to a property link on altitude
       this.updateSkyGradient();
+
+      // Create our cloud nodes
+
+      // Define these constants for convenience
+      var cloudMarginY = FallingObjectsConstants.MB_CLOUD_MARGIN_Y;
+      var cloudMarginYVariance = FallingObjectsConstants.MB_CLOUD_MARGIN_Y_VARIANCE;
+
+      // These variable will help with generating the clouds
+      var lastBotY = 0;  // bottom y coordinate of the last cloud node to be created (set initially to min value of zero, which is top of screen)
+      var posOnLeftCounter = 0;  // which side of the center the cloud should be placed on (if the counter is divisible by 2, will be on the left, otherwise on the right)
+      var counter = 0;
+
+      // While we still have sky left to create a cloud
+      while ( lastBotY <= this.sky.height ) {
+
+        // Determine the initial top Y value of the next cloud to be created, based on margins and the previous cloud's bottom y value
+        var initialTopY = lastBotY + cloudMarginY + ( Math.random() * cloudMarginYVariance );  // Add variance to the margin in the range of 0 to cloudMarginYVariance
+        // Determine the initial X value
+        var initialCenterX = this.genCloudNodeXPos( center.x, posOnLeftCounter % 2 === 0 );
+
+        // Get a random, initial scale
+        var initialScale = this.genCloudNodeScale();
+
+        // Construct the cloud
+        var newCloudNode = this.createCloudNode( initialCenterX, initialTopY, initialScale );
+        this.cloudContainerNode.addChild( newCloudNode );
+
+        // Set these for the next iteration
+        posOnLeftCounter += 1;
+        lastBotY = newCloudNode.getBottom();
+
+        // TODO: Remove this and test
+        counter += 1;
+        if ( counter > 1 ) { break; }
+      }
+
+    },
+
+    /**
+     * Reset the moving background (places clouds at initial position)
+     */
+    reset: function() {
+
+      for ( cloudNode in this.cloudContainerNode.children ) {
+        cloudNode.reset();
+      }
 
     }
 
