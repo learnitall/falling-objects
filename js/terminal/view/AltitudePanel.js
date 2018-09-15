3fd9a5be20b168ea0272791657d466b47b3d988a
Ryan Drew <ryan@thedrews.org>
2018-09-15 15:41:03 -0600

    Altitude slider now fits inside the correct control panel width

--- js/terminal/view/AltitudePanel.js
+++ js/terminal/view/AltitudePanel.js
@@ -10,6 +10,7 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var Dimension2 = require( 'DOT/Dimension2' );
   var fallingObjects = require( 'FALLING_OBJECTS/fallingObjects' );
   var FallingObjectsConstants = require( 'FALLING_OBJECTS/common/FallingObjectsConstants' );
   var HSlider = require( 'SUN/HSlider' );
@@ -35,7 +36,17 @@ define( function( require ) {
     var controlPanelsAlignment = FallingObjectsConstants.CONTROL_PANELS_ALIGNMENT;
     var controlPanelsVerticalSpacing = FallingObjectsConstants.CONTROL_PANELS_VERTICAL_SPACING;
     var altitudeSliderRange = FallingObjectsConstants.AP_SLIDER_RANGE;
-    var altitudeSliderOptions = FallingObjectsConstants.AP_SLIDER_OPTIONS;
+    var altitudeSliderOptions = _.extend( {
+      thumbSize: new Dimension2( 22, 45 ),  // Default values set for thumbSize, put here for reference
+      //            | ----------maxWidth - xmargins---------- |
+      //            | [thumbSize]                 [thumbSize] |
+      // [track boundary min]-------------------------[track boundary max]
+      // When the thumb has been slid all the way to the left, the leftmost part of the track
+      // will lie right in the center of the thumb (same for the right side)
+      // Need to therefore subtract the (half of the width of the thumb) * 2 from the track size
+      // so everything will fit properly
+      trackSize: new Dimension2( maxWidth - ( controlPanelOptions.xMargin * 2 ) - 22, 5 ),  // 5 is the default value here for trackSize height
+    }, FallingObjectsConstants.AP_SLIDER_OPTIONS );
 
     // Create a slider
     var altitudeSlider = new HSlider(
