40047e11ab582a56b103e2efcdb4dbc33e75bf48
Ryan Drew <ryan@thedrews.org>
2018-09-22 13:35:16 -0600

    Added a label to the altitude panel

--- js/terminal/view/AltitudePanel.js
+++ js/terminal/view/AltitudePanel.js
@@ -15,15 +15,19 @@ define( function( require ) {
   var fallingObjects = require( 'FALLING_OBJECTS/fallingObjects' );
   var FallingObjectsConstants = require( 'FALLING_OBJECTS/common/FallingObjectsConstants' );
   var HSlider = require( 'SUN/HSlider' );
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Panel = require( 'SUN/Panel' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
   var pattern0Value1UnitsString = require( 'string!FALLING_OBJECTS/pattern.0Value.1Units' );
+  var pattern0Label1Value2UnitsString = require( 'string!FALLING_OBJECTS/pattern.0Label.1Value.2Units' );
+  var initialAltitudeString = require( 'string!FALLING_OBJECTS/initialAltitude' );
   var kmString = require( 'string!FALLING_OBJECTS/km' );
 
   /**
@@ -117,8 +121,65 @@ define( function( require ) {
       altitudeSlider.addMinorTick( interval * i );
     }
 
+    // Now create a new label to display the drop altitude
+    var labelTextNodeOptions = {
+      font: new PhetFont( { size: FallingObjectsConstants.CONTROL_PANELS_FONT_SIZE } ),
+      maxWidth: maxWidth - ( 2 * FallingObjectsConstants.CONTROL_PANEL_OPTIONS.xMargin )
+    };
+    var labelTextNode = new Text( '', labelTextNodeOptions );
+
+    // Link the text node with the initial altitude property so that it can update on changes
+    fallingObjectsModel.selectedFallingObject.initialAltitudeProperty.link( function( initialAltitude ) {
+
+      // Should we display values?
+      if ( fallingObjectsModel.showValuesProperty.get() ) {
+        // Set text of the node to display the falling object's initial altitude
+        labelTextNode.setText(
+          StringUtils.fillIn( pattern0Label1Value2UnitsString, {
+            label: initialAltitudeString,
+            value: initialAltitude,  // Slider will have already rounded this value
+            units: kmString
+          } )
+        );
+      }
+    } );
+
+    // Link the text with the show values property so that the label will change appropriately when it is toggled
+    fallingObjectsModel.showValuesProperty.link( function( showValues ) {
+
+      if ( !showValues ) {
+        labelTextNode.setText( initialAltitudeString );
+      } else {
+        // Cause the above initialAltitudeProperty link to trigger, updating the label to show values
+        fallingObjectsModel.selectedFallingObject.initialAltitudeProperty.setValueAndNotifyListeners(
+          fallingObjectsModel.selectedFallingObject.initialAltitudeProperty.get()
+        );
+      }
+
+    } );
+
+    // Add the text to a VBox with an HStrut so we can set a persistent max width
+    var labelNode = new VBox( {
+      align: FallingObjectsConstants.CONTROL_PANELS_ALIGNMENT,
+      children: [
+        labelTextNode,
+        new HStrut( labelTextNodeOptions.maxWidth )
+      ]
+    } );
+
+    // Create a VBox to add the slider and labelNode into
+    var altitudePanelElements = new VBox( {
+      resize: true,
+      align: FallingObjectsConstants.CONTROL_PANELS_ALIGNMENT,
+      spacing: FallingObjectsConstants.CONTROL_PANELS_VERTICAL_SPACING,
+      children: [
+        altitudeSlider,
+        labelNode
+      ]
+    } );
+
     // Create the panel
-    var altitudePanel = new Panel( altitudeSlider, controlPanelOptions );
+    var altitudePanel = new Panel( altitudePanelElements, controlPanelOptions );
 
     this.addChild( altitudePanel );
 
